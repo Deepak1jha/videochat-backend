cba818c53048c3ded295a1b05dc00f548aa8c40f
Deepak1jha <ddjha171@gmail.com>
2021-04-07 21:57:47 +0530

    Backend final

--- index.js
+++ index.js
@@ -13,3 +13,22 @@ const PORT = process.env.PORT || 5000;
 app.get('/', ((req, res, next) => {
     res.send("Server is running");
 }));
+
+io.on('connection', (socket) => {
+
+    socket.emit('me', socket.id);
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('callEnded');
+    });
+
+    socket.on('callUser', ({userToCall, signalData, form, name}) => {
+        io.to(userToCall).emit("callUser", {signal: signalData, form, name});
+    });
+
+    socket.on('answerCall', (data) => {
+        io.to(data.to).emit("callAccepted", data.signal);
+    });
+})
+
+server.listen(PORT, () => console.log(`Server started on port ${PORT}`))
