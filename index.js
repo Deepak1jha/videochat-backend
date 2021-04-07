f77fbdfabc01fae52b2d0efc0198be1cff77c22d
Deepak1jha <ddjha171@gmail.com>
2021-04-07 21:41:30 +0530

    First route setup

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const app = require('express')();
 const server = require("http").createServer(app);
 const cors = require('cors');
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "*",
+        method: ["GET", "POST"]
+    }
+});
+app.use(cors());
+const PORT = process.env.PORT || 5000;
+
+app.get('/', ((req, res, next) => {
+    res.send("Server is running");
+}));
